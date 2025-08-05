39841c3d3d09d2df23edf3e155081172dd0d4b73
AllenGleeson <allen_gleeson@hotmail.com>
2025-08-05 23:41:56 +0100

    Add navigation types for routing and navigation props

---
+++ src/types/navigation.ts
@@ -0,0 +1,16 @@
+export type RootTabParamList = {
+  Home: undefined;
+  Search: undefined;
+  Profile: undefined;
+  Settings: undefined;
+};
+
+export type RootStackParamList = {
+  Main: undefined;
+  // Add other stack screens here as needed
+};
+
+export type NavigationProps = {
+  navigation: any;
+  route: any;
+}; 
