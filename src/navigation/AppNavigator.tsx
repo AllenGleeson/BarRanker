0d036009facf45941c8e4ba10a4bce6039b4b55c
AllenGleeson <allen_gleeson@hotmail.com>
2025-08-05 23:41:13 +0100

    Refactor App component and set up navigation with AppNavigator

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TabNavigator from './TabNavigator';
+import { RootStackParamList } from '../types/navigation';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const AppNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Main" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator; 
